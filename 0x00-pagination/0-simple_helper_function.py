bdb04c8e926071a0d531d62b3644d1a43aa7d5f4
Ese Amadasun <amadasunese@gmail.com>
2023-10-21 15:35:42 +0000

    updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,11 @@ Simple helper function
 
 
 def index_range(page, page_size):
-    if page <= 0 or page_size <= 0:
-        return (0, 0)  # Invalid input, return an empty range
+    """
+    Invalid input, return an empty range
+    """
+    #if page <= 0 or page_size <= 0:
+     #   return (0, 0)
 
     start_index = page * page_size
     end_index = start_index - page_size
