50ab6fdfd86c5d436f448e2ddbcca4bf2b1d7e21
Ese Amadasun <amadasunese@gmail.com>
2023-10-24 20:41:20 +0100

    updated

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python3
-"""
-Basic dictionary
-"""
+#!/usr/bin/python3
+"""  Basic dictionary """
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    def __init__(self):
-        super().__init__()
-
+class BasicCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system doesn’t have limit """
     def put(self, key, item):
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Find the first item that was added (FIFO)
-                first_key = next(iter(self.cache_data))
-                print(f"DISCARD: {first_key}\n")
-                del self.cache_data[first_key]
+        """ Assign to the dictionary """
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
