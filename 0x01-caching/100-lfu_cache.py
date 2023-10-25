a165f2767167309725cd962b8fbfb84d9a0f92ea
Ese Amadasun <amadasunese@gmail.com>
2023-10-25 19:30:47 +0100

    fix pycodestyle error

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -16,49 +16,53 @@ class LFUCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Add a key-value pair to the cache
+        Add a key-value pair to the cache.
         """
-        if key in self.least_recently_used_cache:
-            del self.least_recently_used_cache[key]
+        if key is None or item is None:
+            return
 
-        if len(self.least_recently_used_cache) > self.MAX_ITEMS - 1:
-            min_value = min(self.least_frequently_used_cache.values())
-            lfu_keys = [k for k, v in self.least_frequently_used_cache.items()
-                        if v == min_value]
-
-            if len(lfu_keys) == 1:
-                discarded_key = lfu_keys[0]
-            else:
-                for k in self.least_recently_used_cache:
-                    if k in lfu_keys:
-                        discarded_key = k
-                        break
-
-            print("DISCARD:", discarded_key)
-            self.least_recently_used_cache.pop(discarded_key)
-            del self.least_frequently_used_cache[discarded_key]
-
-        self.least_recently_used_cache[key] = item
-        self.least_recently_used_cache.move_to_end(key)
-
-        if key in self.least_frequently_used_cache:
-            self.least_frequently_used_cache[key] += 1
-        else:
-            self.least_frequently_used_cache[key] = 1
-
-        self.cache_data = dict(self.least_recently_used_cache)
+        self._remove_if_needed()
+        self._add_to_cache(key, item)
 
     def get(self, key):
         """
         Retrieve the value for a given key.
         """
         if key in self.least_recently_used_cache:
-            value = self.least_recently_used_cache[key]
             self.least_recently_used_cache.move_to_end(key)
+            self._increment_lfu_count(key)
+            return self.least_recently_used_cache[key]
 
-            if key in self.least_frequently_used_cache:
-                self.least_frequently_used_cache[key] += 1
-            else:
-                self.least_frequently_used_cache[key] = 1
+    def _remove_if_needed(self):
+        if len(self.least_recently_used_cache) > self.MAX_ITEMS - 1:
+            min_lfu_count = min(self.least_frequently_used_cache.values())
+            lfu_keys = [k for k, v in self.least_frequently_used_cache.items()
+                        if v == min_lfu_count]
 
-            return value
+            discarded_key = next((k for k in self.least_recently_used_cache
+                                  if k in lfu_keys), lfu_keys[0])
+
+            self.least_recently_used_cache.pop(discarded_key)
+            del self.least_frequently_used_cache[discarded_key]
+            print("DISCARD:", discarded_key)
+
+    def _add_to_cache(self, key, item):
+        self.least_recently_used_cache[key] = item
+        self.least_recently_used_cache.move_to_end(key)
+        self._increment_lfu_count(key)
+
+    def _increment_lfu_count(self, key):
+        if key in self.least_frequently_used_cache:
+            self.least_frequently_used_cache[key] += 1
+        else:
+            self.least_frequently_used_cache[key] = 1
+
+
+if __name__ == '__main__':
+    # Sample usage
+    cache = LFUCache()
+    cache.put(1, "A")
+    cache.put(2, "B")
+    print(cache.get(1))  # Output: "A"
+    cache.put(3, "C")
+    print(cache.get(2))  # Output: "B"
